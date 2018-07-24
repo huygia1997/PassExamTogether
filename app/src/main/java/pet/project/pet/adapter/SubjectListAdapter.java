f2ae58dd9e4dfddf98553c1d414c684cebfcfbfe
unknown <phukhanhnguyen1997@gmail.com>
2018-07-24 15:03:54 +0700

    abcxyz

--- app/src/main/java/pet/project/pet/adapter/SubjectListAdapter.java
+++ app/src/main/java/pet/project/pet/adapter/SubjectListAdapter.java
@@ -1,11 +1,15 @@
 package pet.project.pet.adapter;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.Serializable;
@@ -23,13 +27,19 @@ public class SubjectListAdapter extends BaseAdapter implements Serializable {
     private List<Subject> listData;
     private LayoutInflater layoutInflater;
     private Context context;
-
+    customButtonListener customListner;
     public SubjectListAdapter(List<Subject> listData, Context context) {
         this.listData = listData;
         this.context = context;
         layoutInflater = LayoutInflater.from(context);
     }
+    public interface customButtonListener{
+        public void onButtonClick(int position);
+    }
 
+    public void setCustomButtonListener(customButtonListener listener){
+        this.customListner = listener;
+    }
     @Override
     public int getCount() {
         return listData.size();
@@ -46,37 +56,62 @@ public class SubjectListAdapter extends BaseAdapter implements Serializable {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         ViewHolder viewHolder;
         if (convertView == null) {
             convertView = layoutInflater.inflate(R.layout.subject_item_layout, null);
             viewHolder = new ViewHolder();
             viewHolder.subjectNameView = (TextView) convertView.findViewById(R.id.textView_subjectName);
             viewHolder.subjectUserCreatedView = (TextView) convertView.findViewById(R.id.textView_subjectUserCreated);
-            //viewHolder.subjectTimeCreatedView = (TextView) convertView.findViewById(R.id.textView_subjectTimeCreated);
+            viewHolder.subjectEditView = (ImageButton) convertView.findViewById(R.id.button_editSubject);
+            viewHolder.subjectEditView.setImageResource(R.drawable.index);
+            viewHolder.subjectSubIdView = (TextView) convertView.findViewById(R.id.textView_subjectSubId);
             convertView.setTag(viewHolder);
         } else {
             viewHolder = (ViewHolder) convertView.getTag();
         }
-        Subject subject = this.listData.get(position);
+        final Subject subject = this.listData.get(position);
         viewHolder.subjectNameView.setText(subject.getSubName());
-
         if (subject.getDisplayName().isEmpty() || subject.getDisplayName() == "") {
             viewHolder.subjectUserCreatedView.setText(subject.getUsername());
         } else {
             viewHolder.subjectUserCreatedView.setText(subject.getDisplayName());
         }
+        viewHolder.subjectEditView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //anh lam edit o day
+                Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.dialog_edit_subject);
 
+                final TextView txt_edit_name = (TextView) dialog.findViewById(R.id.txt_edit_subject_name);
+                final TextView txt_edit_code = (TextView) dialog.findViewById(R.id.txt_edit_subject_name);
 
-        if (subject.getCreatedDate() != null) {
-            viewHolder.subjectTimeCreatedView.setText(subject.getCreatedDate().toString());
+                txt_edit_name.setText(subject.getSubName()+ "");
+                txt_edit_code.setText(subject.getSubCode()+ "");
+                dialog.show();
+                Button buttonSubmit = (Button) dialog.findViewById(R.id.btn_submit_edit_subject);
+                buttonSubmit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //anh edit o day ne.
+                        txt_edit_name.setText(subject.getSubName());
+                        txt_edit_code.setText(subject.getSubCode());
+                    }
+                });
+            }
+        });
+        if(!(subject.getSubCode() == null)){
+            viewHolder.subjectSubIdView.setText(subject.getSubCode() + "");
         }
+
         return convertView;
     }
 
     static class ViewHolder {
         TextView subjectNameView;
         TextView subjectUserCreatedView;
-        TextView subjectTimeCreatedView;
+        TextView subjectSubIdView;
+        ImageButton subjectEditView;
     }
 }
